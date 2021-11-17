aa88dd4dd673351e416ac6990c47285a78a78455
EJ_Huang <ej@enstar.com.tw>
2021-11-17 16:17:46 +0800

    PC view & scroll
    # Conflicts:
    #	public/index.html
    #	src/components/Class.vue
    #	src/components/Footer.vue
    #	src/components/Formula.vue
    #	src/components/Share.vue

--- src/components/Formula.vue
+++ src/components/Formula.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class=" main h-100 bg-dark_black  text-2xl mt-12 pt-12">
-        <div class="box flex  bg-secondary text-2xl text-white mt-5 py-1  mx-5 my-5  rounded-md p-4">
+    <div class="main h-100 items-center bg-dark_black  text-2xl mt-12 pt-12">
+        <div class="box flex items-center justify-center h-12 bg-secondary text-2xl text-white mt-5 py-1 px-1  my-5  rounded-md p-4">
             <img src="../assets/star1.png" alt="">
-            <h1 class="flex content-center mt-12">IMPORTANT</h1>
+            <h1 class="flex content-center mt-4">IMPORTANT</h1>
         </div>
-        <ol class="list-disc p-10 text-left font-black text-secondary">
+        <ol class="list-disc lg:list-none  p-10 text-left lg:text-center font-black text-secondary">
             <li>短線交易的秘密公式</li>
             <span> 將幫助你建立清晰有架構的交易方式，並讓你有信心在股市中達成穩定獲利 !</span>
             <li>短線交易的秘密公式</li>
@@ -12,89 +12,106 @@
             <li>短線交易的秘密公式</li>
             <span> 將帶領你，運用小資金操作股市，逐步滾起百萬獲利！</span>
         </ol>
-    <div class="main text-left p-2 leading-10">
+    <div class="main text-left lg:text-center p-2 leading-10">
         <h1 class="font-bold text-2xl text-white">這套短線交易公式已被應用於</h1>
     </div>
         <hr class="mt-2 py-4">
-        <div class="main2">
-            <div class="box flex text-secondary">
+        <div class="main2 flex flex-col">
+            <div class="box flex  text-secondary"  data-aos="fade-down" data-aos-duration="2000">
                 <h1>01</h1>
                 <span>台股股票</span>
             </div>  
-            <div class="content flex p-2">
-                <div class="content-1 text-xl  flex flex-col pl-2">
+            <div class="content flex justify-evenly p-2">
+                <div class="content-1 text-xl items-center flex flex-col pl-2"  data-aos="fade-right" data-aos-duration="1500">
                     <p>尋找飆股學生曾抓出近<span>300%獲利飆股 !</span></p>
                     <img src="../assets/form1.png" alt="">
                 </div>
-                <img src="../assets/form.png" alt="">
+                <img src="../assets/form.png"  data-aos="fade-left" data-aos-duration="2000" alt="">
                 <!-- <div class="content-2 pr-2">
                     <img src="../assets/foot.png" alt="">
                 </div> -->
             </div>         
         </div>
         <div class="main2">
-            <div class="box flex text-secondary">
+            <div class="box flex  text-secondary" data-aos="fade-down" data-aos-duration="2000">
                 <h1>02</h1>
                 <span>台股台指期當沖</span>
             </div>  
-            <div class="content flex pt-8">
-                <div class="content-1 text-xl  flex pl-2">
+            <div class="content flex justify-evenly pt-8">
+                <div class="content-1 text-xl items-center  flex pl-2" data-aos="fade-right" data-aos-duration="1500">
                     <p>分析盤勢，清楚抓出近期走向，台股一次下殺，<span>獲利達1000點！</span></p>
-                    <img src="../assets/form2.png" alt="">
+                    
                 </div>
+                <img src="../assets/form2.png"  data-aos="fade-left" data-aos-duration="2000" alt="">
             </div>         
         </div>
-        <div class="main2">
+        <div class="main2 pt-8">
             <div class="box flex text-secondary">
                 <h1>03</h1>
                 <span>台股台指期波段</span>
             </div>  
-            <div class="content flex pt-8">
+            <div class="content flex justify-center pt-8 mb-10">
                 <div class="content-1 text-xl  flex pl-2">
                     <p>當你短線交易做的成功了，來挑戰波段單，獲利更驚人</p>
                 </div>
             </div>         
         </div>
         <div class="main2">
-            <div class="box flex text-secondary">
+            <div class="box flex  text-secondary">
                 <h1>04</h1>
                 <span>美股當沖</span>
             </div>  
-            <div class="content flex pt-8">
+            <div class="content flex justify-center pt-8">
                 <div class="content-1 text-xl  flex pl-2">
                     <p>當初這套公式，就是在美股當沖中練成，自然能應用到美股上！</p>
                 </div>
             </div>         
         </div>
-        <div class="main2">
+        <div class="main2 pt-8">
             <div class="box flex text-secondary">
                 <h1>05</h1>
                 <span>美股個股交易</span>
             </div>  
-            <div class="content flex pt-8">
+            <div class="content flex justify-center pt-8">
                 <div class="content-1 text-xl  flex pl-2">
                     <p>有在關注特斯拉？蘋果？想交易美股，也可以透過公式解析！</p>
                 </div>
             </div>         
         </div>
-    <div class="box1 p-4">
-        <div class="flex side-box justify-center bg-black text-2xl text-white mt-12  py-1 px-2  mx-5 my-5 rounded-md p-5">
-            <img src="../assets/star2.png" alt="">
-            <h1 class="flex content-center pt-2">限額!名額有限!</h1>
-        </div>
-        
-        <p class="text-white">短線交易的秘密公式</p>
-        <p class="text-white"> 開始規劃出明確走勢</p>
-        <p class="text-secondary">用三萬小資金，創造百萬獲利!</p>
+    <div class="row main flex md:flex-col items-center pt-12" data-aos="flip-right" data-aos-duration="1500"
+     data-aos-delay='1500' >
+        <div class="box1 p-4">
+            <div class="flex side-box justify-center bg-black text-2xl text-white mt-12  py-1 px-2   mx-5 my-5 lg:my-10 rounded-md p-5">
+                <img src="../assets/star2.png" alt="">
+                <h1 class="flex content-center pt-2">限額!名額有限!</h1>
+            </div>
+            
+            <p class="text-white">短線交易的秘密公式</p>
+            <p class="text-white"> 開始規劃出明確走勢</p>
+            <p class="text-secondary">用三萬小資金，創造百萬獲利!</p>
 
-        <button class="bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-12  rounded mt-8">
-        / 搶佔免費講座席次
-        </button>
+            <button class="btn bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-12 ml-12 lg:ml-24  rounded mt-8 lg:mt-16">
+            <img src="../assets/arrorw.png" alt="">/ 搶佔免費講座席次
+            </button>
+        </div>
     </div>
-  </div>
+</div>
   
 </template>
 
+
+<script>
+import AOS from 'aos/dist/aos.css'
+export default {
+  name: 'Formula',
+  props: {
+    msg: String
+  },
+    mounted: function(){
+      AOS
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 
@@ -140,7 +157,7 @@
     background-image: url('../assets/foot2.png');
     background-size: cover;
     height: 50vh;
-    width: 100%;
+    // width: 100%;
     background-repeat: no-repeat;
     padding: 10px;
 
@@ -148,6 +165,42 @@
         img{
             width: 6rem;
             padding: 10px;
+        }
+    }
+}
+.btn{
+    img{
+        width: 10%;
+    }
+}
+@media (min-width: 1280px) {
+    .main{
+        .box{
+            position: relative;
+            left: 40%;
+            width: 20rem;
+            padding: 10px;
+        }
+        .box1{
+            background-size: contain;
+            height: 40rem;
+            width: 30rem;
+        }
+    }
+    .main2{
+        .box{
+            border-radius: 1%;
+        }
+        // display: flex;
+        // justify-content: space-around;
+        // .box{
+        //     justify-content: space-around;
+        // }
+        .content{
+            img{
+                justify-items: center;
+                width: 30rem;
+            }
         }
     }
 }
--- src/components/Share.vue
+++ src/components/Share.vue
@@ -1,81 +1,84 @@
 <template>
-<div class=" main">
-  <div class="flex md:flex-col flex-wrap md:content-around p-5">
-      <div class=" justify-center bg-secondary text-lg w-15 text-white mt-5 rounded-md p-2">
-          <h1 class="flex content-center font-bold">現場分享</h1>
+<div class="main items-center   ">
+  <div class="flex md:flex-col flex-wrap p-5">
+      <div class="box-1 justify-center bg-secondary text-lg  text-white mt-5 rounded-md p-2">
+          <h1 class="flex font-bold">現場分享</h1>
       </div>
-    <div class="text-3xl text-left pt-2">現場講座中將與你分享五大實戰要點</div>
+    <div class="text-3xl text-left lg:text-center pt-2">現場講座中將與你分享五大實戰要點</div>
   </div>
   <hr class="mt-2">
-    <div class="main2 pt-8">
+    <div class="main2 flex flex-col pt-8">
         <div class="box flex text-secondary">
             <h1>01</h1>
             <span>台股台指期當沖</span>
         </div>    
-        <div class="content flex pt-8">
+        <div class="content flex justify-center pt-8">
             <div class="content-1 flex pl-2">
                 <p>如何透過公式，清楚的抓出任何投資商品的走勢？最精采的內容，線上不能公開的秘密，我將在現場跟你分享。</p>
             </div>
         </div>      
     </div>
-    <div class="main2 pt-8">
+    <div class="main2 flex flex-col pt-8">
         <div class="box flex text-secondary">
             <h1>02</h1>
             <span>提高勝率的八大型態</span>
         </div>    
-        <div class="content flex pt-8">
+        <div class="content flex justify-center pt-8">
             <div class="content-1 flex pl-2">
                 <p>觀察超過3萬多張盤勢之後，我歸納出的8大型態，用對了，可以大幅度提高你的交易勝率！</p>
             </div>
         </div>      
     </div>
-    <div class="main2 pt-8">
-        <div class="box flex text-secondary">
+    <div class="main2 flex flex-col pt-8">
+        <div class="box flex justify-center text-secondary">
             <h1>03</h1>
             <span>成功交易人的四大特質與十大特性</span>
         </div>    
-        <div class="content flex pt-8">
+        <div class="content justify-center flex pt-8">
             <div class="content-1 flex pl-2">
                 <p>在投資領域，技術很重要，但是心態更重要，如果你在投資路上績效不理想，這些特質與特性可以幫助你重新檢視自己。</p>
             </div>
         </div>      
     </div>
-    <div class="main2 pt-8">
+    <div class="main2 flex flex-col pt-8">
         <div class="box flex text-secondary">
             <h1>04</h1>
             <span>停損的藝術，計算盈虧讓每次交易都俱備最佳cp值！</span>
         </div>    
-        <div class="content flex pt-8">
+        <div class="content justify-center flex pt-8">
             <div class="content-1 flex pl-2">
                 <p>盈虧比，這個重要觀念，很多操作十多年的投資人都可能不知道，搞懂盈虧比，抓住最佳停損時機，才是交易能穩定獲利的王道。</p>
             </div>
         </div>      
     </div>
-    <div class="main2 pt-8">
+    <div class="main2 flex flex-col pt-8">
         <div class="box flex text-secondary">
             <h1>05</h1>
             <span>上班族也能操盤！特殊高勝率操盤法！每天10分鐘輕鬆交易！</span>
         </div>    
-        <div class="content flex pt-8">
+        <div class="content justify-center flex pt-8">
             <div class="content-1 flex pl-2">
                 <p>上班族上班無法操盤怎麼做？完全不用擔心，你可以運用早上10分鐘，簡單的方式快速搞定交易。</p>
             </div>
         </div>      
     </div>
 
-    <div class="box1 p-4">
-        <div class="flex side-box justify-center bg-black text-2xl text-white mt-12  py-1 px-2  mx-5 my-5 rounded-md p-5">
-            <img src="../assets/star2.png" alt="">
-            <h1 class="flex content-center pt-2">限額!名額有限!</h1>
+    <div class="row main flex md:flex-col items-center pt-12" data-aos="flip-right" data-aos-duration="1500"
+     data-aos-delay='1500' >
+        <div class="box1 p-4 items-center">
+            <div class="flex side-box justify-center bg-black text-2xl text-white mt-12  py-1 px-2  mx-5 my-5 rounded-md p-5">
+                <img src="../assets/star2.png" alt="">
+                <h1 class="flex content-center pt-2">限額!名額有限!</h1>
+            </div>
+            <div class="inside flex justify-center items-center p-2">
+                <div class="side box-content h-2.5 w-16 p-4 border-2 text-white">報名即贈</div>
+                <img src="../assets/foot.png" alt="">
+            </div>
+            <p class="text-secondary">諒師獨家型態學教學影片</p>
+            <button class="btn bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-12 ml-12 lg:ml-24  rounded mt-8 lg:mt-16">
+            <img src="../assets/arrorw.png" alt="">/ 搶佔免費講座席次
+            </button>
         </div>
-        <div class="inside flex justify-center items-center p-2">
-            <div class="side box-content h-2.5 w-16 p-4 border-2 text-white">報名即贈</div>
-            <img src="../assets/foot.png" alt="">
-        </div>
-        <p class="text-secondary">諒師獨家型態學教學影片</p>
-        <button class="bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-12  rounded mt-8">
-        / 搶佔免費講座席次
-        </button>
     </div>
 </div>
 </template>
@@ -124,5 +127,32 @@
                 padding: 10px;
             }
         }
+    }
+    .btn{
+        img{
+            width: 10%;
+        }
+    }
+    @media (min-width: 1280px) {
+        .main{
+            .box{
+                position: relative;
+                left: 40%;
+                width: 20rem;
+                padding: 10px;
+            }
+            .box1{
+                background-size: contain;
+                height: 30rem;
+                width: 30rem;
+            }
+            .box-1{
+                position: relative;
+                left: 40%;
+                width: 20rem;
+                padding: 10px;
+            }
+        }
+
     }
 </style>
